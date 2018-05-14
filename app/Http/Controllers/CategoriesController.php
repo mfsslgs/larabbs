f3002d90ea1db937c537f53b961d5e8396c638b1
vagrant <vagrant@homestead>
2018-05-14 11:42:56 +0000

    topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,9 +9,9 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category){
+    public function show(Category $category,Request $request,Topic $topic){
 
-      $topics = Topic::with('user','category')->where('category_id',$category->id)->paginate(30);
+      $topics =$topic->withOrder($request->order)->where('category_id',$category->id)->paginate(30);
       return view('topics.index',compact('topics','category'));
 
     }
