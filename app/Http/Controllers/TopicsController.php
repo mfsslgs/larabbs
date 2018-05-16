c3f5278d5b8cdcc472ca63c5dfc172b94f317578
vagrant <vagrant@homestead>
2018-05-16 03:53:41 +0000

    user can uploda topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -62,4 +62,25 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+  public function uploadImage(Request $request,ImageUploadHandler $uploader){
+
+    $data = [
+      'success'   => false,
+      'msg'       => '上传失败！',
+      'file_path' => '',
+    ];
+
+    if($file = $request->upload_file){
+      $result = $uploader->save($request->upload_file,'topics',\Auth::id(),1024);
+      if($result){
+        $data['file_path'] = $result['path'];
+        $data['msg'] = '上传成功！';
+        $data['success'] = true;
+      }
+    }
+
+    return $data;
+    
+  }
 }
