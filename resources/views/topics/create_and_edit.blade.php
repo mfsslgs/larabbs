57debeb6a7ebae1f9773b69b3fe1e468d91f202d
vagrant <vagrant@homestead>
2018-05-16 15:36:12 +0000

    topic detail page

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -74,10 +74,10 @@
             textarea: $('#editor'),
             upload:{
               url:'{{route('topics.upload_image')}}',
-              param:{_token:'{{csrf_token()}}'},
+              params:{_token:'{{csrf_token()}}'},
               fileKey:'upload_file',
               connectionCount:3,
-              leaveConfirm:'文件上传中，关闭此页面将取消上传。'
+              leaveConfirm:'文件上传中，关闭此页面将取消上传。',
             },
             pasteImage:true,
         });
