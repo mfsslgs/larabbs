da0ccf93b86c0ce9c036c51ddec19517651d819b
vagrant <vagrant@homestead>
2018-05-17 14:00:41 +0000

    user can create reply

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
     \Carbon\Carbon::setLocale('zh');
-    
+
     }
 
     /**
