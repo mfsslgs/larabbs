306c1962021cbdb4ff97acfe301806ec262273ba
vagrant <vagrant@homestead>
2018-05-21 07:35:15 +0000

    siedbar links

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use App\Models\Category;
 use App\Handlers\ImageUploadHandler;
 use Auth;
 use App\Models\User;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -18,13 +19,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request,Topic $topic,User $user)
-	{
-		$topics = $topic->withOrder($request->order)->paginate(20);
-    $active_users = $user->getActiveUsers();
-    //dd($active_users);
-		return view('topics.index', compact('topics','active_users'));
-	}
+    public function index(Request $request, Topic $topic, User $user, Link $link)
+      {
+          $topics = $topic->withOrder($request->order)->paginate(20);
+          $active_users = $user->getActiveUsers();
+          $links = $link->getAllCached();
+
+          return view('topics.index', compact('topics', 'active_users', 'links'));
+      }
 
     public function show(Topic $topic,Request $request)
     {
